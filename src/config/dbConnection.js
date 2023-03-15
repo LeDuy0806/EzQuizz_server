6c23d1a1257a65a51527f34ea6fdabe8d9c58211
Lê Duy <duylangtu931@gmail.com>
2023-03-15 19:35:51 +0700

    Handle mongoDb error

--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const connectDb = async () => {
     try {
-        const connect = await mongoose.connect(process.env.CONNECTION_STRING);
+        const connect = await mongoose.connect(process.env.CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log(
             'Database connected: ',
             connect.connection.host,
