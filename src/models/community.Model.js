0332cd7015f1999193b051df43e8f49802b2ffb3
AnhQuoc189 <binbin18092003@gmail.com>
2023-06-26 18:36:56 +0700

    add community

---
+++ src/models/community.Model.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const communitySchema = new mongoose.Schema({
+    backgroundImage: {
+        type: String
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    quizzes: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'Quiz'
+    },
+    tags: {
+        type: String
+    },
+    users: {
+        type: [mongoose.Schema.Types.ObjectId],
+        ref: 'User'
+    },
+    creatorId: {
+        type: mongoose.Schema.Types.ObjectId
+    },
+    creatorName: {
+        type: String
+    }
+});
+
+module.exports = mongoose.model('Community', communitySchema);
