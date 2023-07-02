ff3a100e57f9653e131c1d56f3bb1a621ca2cf5f
AnhQuoc189 <binbin18092003@gmail.com>
2023-07-03 04:04:40 +0700

    history user

--- src/controllers/leaderboard.controller.js
+++ src/controllers/leaderboard.controller.js
@@ -3,6 +3,56 @@ const Leaderboard = require('../models/leaderBoard.Model');
 const Quiz = require('../models/quiz.Model');
 const Game = require('../models/game.Model');
 
+const getHistory = async (req, res) => {
+    // const { id } = req.params;
+    // console.log(req.params);
+    // const games = await Game.find({});
+    // console.log(games);
+    const leaderboards = await Leaderboard.find();
+
+    const games = await Game.find();
+    const leaderboardWithGame = await Promise.all(
+        leaderboards.map(async (leaderboard) => {
+            const game = await Game.find({
+                _id: leaderboard.gameId
+            });
+            return {
+                ...leaderboard._doc,
+                game
+            };
+        })
+    );
+    try {
+        // const gameWithQuiz = await Promise.all(
+        //     games.map(async (game) => {
+        //         const quiz = await Quiz.find({
+        //             _id: game.quizId
+        //         });
+        //         return {
+        //             ...game._doc,
+        //             quiz
+        //         };
+        //     })
+        // );
+
+        const leaderboardWithGameQuiz = await Promise.all(
+            leaderboardWithGame.map(async (leaderboard) => {
+                const quiz = await Quiz.find({
+                    _id: leaderboard.game[0].quizId
+                });
+                return {
+                    ...leaderboard,
+                    quiz
+                };
+            })
+        );
+
+        res.status(200).json(leaderboardWithGameQuiz);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 const createLeaderboard = async (req, res) => {
     const { gameId, playerResultList, pin } = req.body;
 
@@ -116,6 +166,7 @@ const updateCurrentLeaderboard = async (req, res) => {
 };
 
 module.exports = {
+    getHistory,
     createLeaderboard,
     deleteLeaderboard,
     getLeaderboard,
